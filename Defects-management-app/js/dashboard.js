80d4c0cb88bccb8afa8f850b19f2d1f59be2e7f1
Benjamin Mak <121108280+benjamin-mak@users.noreply.github.com>
2023-06-13 11:59:55 +0800

    Shows a popup window when add project button is clicked
    Add project button on click - added a pop up window that asks the user to input the project name. Able to close the popup window as well.

--- Defects-management-app/js/dashboard.js
+++ Defects-management-app/js/dashboard.js
@@ -1,12 +1,45 @@
-// Event handlers object
+// Event handlers object //
 const eventHandlers = {
   // Logouts and redirects to login page
   logout: (event) => {
     event.target.href = "../login.html";
   },
+
+  // Returns to the home dashboard page
+  dashboardHome: (event) => {
+    event.target.parentNode.href = "dashboard-html/dashboard.html";
+  },
+
+  // Opens the create a new project popup
+  addProject: () => {
+    document.querySelector("#project-name-popup-id").classList.add("active");
+    document.querySelector("#overlay").classList.add("active");
+  },
+
+  // Closes the create a new project popup
+  closeAddProject: () => {
+    document.querySelector("#project-name-popup-id").classList.remove("active");
+    document.querySelector("#overlay").classList.remove("active");
+  },
 };
 
-// Add logout event to logout button
+// Buttons //
+// Logout button - On click logouts and returns to login page
 document
   .querySelector("#logout-btn")
   .addEventListener("click", eventHandlers.logout);
+
+// Home button - On click, returns to home dashboard
+document
+  .querySelector("#home-btn")
+  .addEventListener("click", eventHandlers.dashboardHome);
+
+// Add project button - on click, creates a new project popup
+document
+  .querySelector("#add-project")
+  .addEventListener("click", eventHandlers.addProject);
+
+// Project name popup window close button
+document
+  .querySelector("#project-name-popup-close-btn")
+  .addEventListener("click", eventHandlers.closeAddProject);
